5ddacaa825026d0a610218af455b6f2a5697832b
SAHA-YJB <pkomom123@naver.com>
2023-11-14 17:58:31 +0900

    comment:loader  comment

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -3,6 +3,8 @@ import EventsList from "../components/EventsList";
 
 // 페이지가 이동되기 시작할때 로더 호출
 // 실제로 가기 전 호출
+// 브라우저에서 실행
+// 브라우저 API사용 가능
 export const loader = async () => {
   const response = await fetch("http://localhost:8080/events");
   if (!response.ok) {
