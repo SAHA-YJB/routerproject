24e69aeeff63ac8c73f3e872f1d5024442ed10fb
SAHA-YJB <pkomom123@naver.com>
2023-11-14 17:42:24 +0900

    features: loading state check

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,6 +1,8 @@
 import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
+// 페이지가 이동되기 시작할때 로더 호출
+// 실제로 가기 전 호출
 export const loader = async () => {
   const response = await fetch("http://localhost:8080/events");
   if (!response.ok) {
